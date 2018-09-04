4b10cbe12b2238e0c0f6c39e89e0c291a165f28d
Kapil <kapilsomani.h@gmail.com>
2018-09-03 22:42:37 -0400

    First draft

--- automator/automator.pl
+++ automator/automator.pl
@@ -3,29 +3,46 @@
 use strict;
 use warnings;
 
+sub prompt {
+    my $cmd = shift;
+    print "Executing: $cmd\nContinue(y/n)?";
+    my $response = <STDIN>;
+    chomp $response;
+    if (lc $response eq 'y') {
+	return;
+    }
+    else {
+	print "Please enter a valid response\n";
+    prompt($cmd);
+    }
+}
+
 my $myfile = "commands.steps";
 open(my $steps, "<", $myfile)
     or die("Error opening file: ($^E)");
 
 while (my $line = <$steps>) {
     chomp $line;
-    print "\n";
     # If line contains only whitespace; continue
     if ($line =~ /^\s*$/) {
 	next;
     }
     # If printable comment(starts with "##")
     elsif ($line =~ /^##/) {
-	print $line;
+	print "$line\n";
     }
     # If printable comment(starts with "#")
     elsif ($line =~ /^#/) {
 	next;
     }
+    # Execute the command
     else {
-	print "Command: $line";
+	prompt $line;
+	system $line;
     }
 }
+
+
 
 close($steps);
 
